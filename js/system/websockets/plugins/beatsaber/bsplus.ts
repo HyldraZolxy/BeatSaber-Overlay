b08d9fdf145572f343508cc68878e72d11977729
Hyldra Zolxy <hyldra.zolxy@outlook.fr>
2025-01-14 19:36:52 +0100

    Add second WebSocket channels for BSPlus and DataPuller but ...
    If you have any idea how to keep the second websocket connection alive along side the first one, without being killed because the first one or the second one is connected, I take it

--- js/system/websockets/plugins/beatsaber/bsplus.ts
+++ js/system/websockets/plugins/beatsaber/bsplus.ts
@@ -2,24 +2,39 @@ import { WebSocketManager } from '../../websocketsManager';
 import { BSPlus }           from '../../../../games/beatSaber/BSPlus';
 
 export class BsPlusPlugin {
-    private readonly key: string;
-    private manager     : WebSocketManager;
-    private _bsPlus     = new BSPlus();
+    private readonly soloData       : string;
+    private readonly multiplayerData: string;
+    private manager                 : WebSocketManager;
+    private _bsPlus                 = new BSPlus();
 
     constructor(manager: WebSocketManager) {
-        this.manager = manager;
-        this.key     = 'bsplus';
+        this.manager         = manager;
+        this.soloData        = 'bsplus_solodata';
+        this.multiplayerData = 'bsplus_multiplayerdata';
 
         // Initialize the WebSocket with plugin-specific message handler
-        this.manager.initialize(this.key, 'ws://127.0.0.1:2947/socket', this.handleMessage.bind(this));
+        this.manager.initialize(this.soloData, 'ws://127.0.0.1:2947/socket', this.handleSoloDataMessage.bind(this));
+
+        ///TODO: Make it work ... Actually, i don't know how to make it connect along side the first one
+        this.manager.initialize(this.multiplayerData, 'ws://127.0.0.1:2948/socket', this.handleMultiplayerDataMessage.bind(this));
     }
 
     /**
-     * Handles incoming messages for the BSPlus WebSocket.
+     * Handles incoming messages for the BSPlus SoloData WebSocket.
      * @param message The received WebSocket message.
      */
-    private handleMessage(message: string): void {
-        console.log(`BsPlusPlugin Message: ${message}`);
+    private handleSoloDataMessage(message: string): void {
+        console.log(`BsPlusPlugin SoloData Message: ${message}`);
+        // Handle plugin-specific WebSocket message logic here
+        this._bsPlus.dataParser(message)
+    }
+
+    /**
+     * Handles incoming messages for the BSPlus MultiplayerData WebSocket.
+     * @param message The received WebSocket message.
+     */
+    private handleMultiplayerDataMessage(message: string): void {
+        console.log(`BsPlusPlugin MultiplayerData Message: ${message}`);
         // Handle plugin-specific WebSocket message logic here
         this._bsPlus.dataParser(message)
     }
@@ -28,7 +43,15 @@ export class BsPlusPlugin {
      * Sends a message through the WebSocket.
      * @param data The data to send.
      */
-    public sendMessage(data: string): void {
-        this.manager.sendMessage(this.key, data);
+    public sendSoloDataMessage(data: string): void {
+        this.manager.sendMessage(this.soloData, data);
+    }
+
+    /**
+     * Sends a message through the WebSocket.
+     * @param data The data to send.
+     */
+    public sendMultiplayerDataMessage(data: string): void {
+        this.manager.sendMessage(this.multiplayerData, data);
     }
 }
--- js/system/websockets/plugins/beatsaber/datapuller.ts
+++ js/system/websockets/plugins/beatsaber/datapuller.ts
@@ -2,33 +2,56 @@ import { WebSocketManager } from '../../websocketsManager';
 import { DataPuller }       from '../../../../games/beatSaber/dataPuller';
 
 export class DataPullerPlugin {
-    private readonly key: string;
-    private manager     : WebSocketManager;
-    private _dataPuller = new DataPuller();
+    private readonly mapData : string;
+    private readonly liveData: string;
+    private manager          : WebSocketManager;
+    private _dataPuller      = new DataPuller();
 
     constructor(manager: WebSocketManager) {
-        this.manager = manager;
-        this.key     = 'datapuller_mapdata';
+        this.manager  = manager;
+        this.mapData  = 'datapuller_mapdata';
+        this.liveData = 'datapuller_livedata';
 
         // Initialize the WebSocket with plugin-specific message handler
-        this.manager.initialize(this.key, 'ws://127.0.0.1:2946/BSDataPuller/MapData', this.handleMessage.bind(this));
+        this.manager.initialize(this.mapData, 'ws://127.0.0.1:2946/BSDataPuller/MapData', this.handleMapDataMessage.bind(this));
+
+        ///TODO: Make it work ... Actually, i don't know how to make it connect along side the first one
+        this.manager.initialize(this.liveData, 'ws://127.0.0.1:2946/BSDataPuller/LiveData', this.handleLiveDataMessage.bind(this));
     }
 
     /**
-     * Handles incoming messages for the DataPuller WebSocket.
+     * Handles incoming messages for the DataPuller MapData WebSocket.
      * @param message The received WebSocket message.
      */
-    private handleMessage(message: string): void {
+    private handleMapDataMessage(message: string): void {
         console.log(`DataPuller MapData Message: ${message}`);
         // Handle plugin-specific WebSocket message logic here
-        this._dataPuller.dataParser(message, "mapData"); ///TODO: Hardcoded for now
+        this._dataPuller.dataParser(message, "mapData");
+    }
+
+    /**
+     * Handles incoming messages for the DataPuller LiveData WebSocket.
+     * @param message The received WebSocket message.
+     */
+    private handleLiveDataMessage(message: string): void {
+        console.log(`DataPuller LiveData Message: ${message}`);
+        // Handle plugin-specific WebSocket message logic here
+        this._dataPuller.dataParser(message, "liveData");
     }
 
     /**
      * Sends a message through the WebSocket.
      * @param data The data to send.
      */
-    public sendMessage(data: string): void {
-        this.manager.sendMessage(this.key, data);
+    public sendMapDataMessage(data: string): void {
+        this.manager.sendMessage(this.mapData, data);
+    }
+
+    /**
+     * Sends a message through the WebSocket.
+     * @param data The data to send.
+     */
+    public sendLiveDataMessage(data: string): void {
+        this.manager.sendMessage(this.liveData, data);
     }
 }
